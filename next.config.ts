9c746fdab3221d566f97b205e1ac656e622ccc01
Devika Harshey <144525325+devikaharshey@users.noreply.github.com>
2024-12-20 21:18:49 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: true,
+    compiler: {
+        styledComponents: true,
+    },
+    productionBrowserSourceMaps: true,
+    webpack(config, { dev, isServer }) {
+        if (dev && !isServer) {
+            config.devtool = 'cheap-module-source-map';
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
