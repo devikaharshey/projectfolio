f24241586c8c657f944da60a22d21b65fb7d6f43
Devika Harshey <144525325+devikaharshey@users.noreply.github.com>
2025-06-18 12:34:15 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import {ProjectHome} from "@/pages/ProjectHome";
 
 export default function Home() {
   return (
-      <main className="relative bg-black-200 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
         <div className="max-w-7xl w-full">
             <Hero />
             <ProjectRecipe />
